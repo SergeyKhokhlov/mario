4c9511300be0fd0a96174e08f725e495522e45dc
Sergey <serg78kh@yandex.ru>
2019-12-18 17:19:18 +0300

    commit

---
+++ code.py
@@ -0,0 +1,134 @@
+import pygame
+import os
+import sys
+
+pygame.init()
+size = width, height = 700, 650
+screen = pygame.display.set_mode(size)
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+tile_images = {'wall': load_image('box.png'), 'empty': load_image('grass.png')}
+player_image = load_image('mar.png')
+tile_width = tile_height = 50
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
+
+
+def terminate():
+    pygame.init()
+    sys.exit()
+
+
+def start_screen():
+    intro_text = ['ЗАСТАВКА', '',
+                  'ПРАВИЛА ИГРЫ',
+                  'Если в правилах несколько строк',
+                  'приходится выводить их построчно']
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        pygame.display.flip()
+
+
+def load_level(filename):
+    filename = 'data/' + filename
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    max_width = max(map(len, level_map))
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[x][y] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Player(x, y)
+    return new_player, x, y
+
+
+class Camera:
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+
+    def apply(self, obj):
+        obj.rect.x += self.dx
+        obj.rect.y += self.dy
+
+    def update(self, target):
+        self.dx = width // 2 - (target.rect.x + target.rect.w // 2)
+        self.dy = height // 2 - (target.rect.y + target.rect.h // 2)
+
+
+player = None
+
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+player, level_x, level_y = generate_level(load_level('level1.txt'))
+camera = Camera()
+background_color = load_image('fon.jpg')
+screen.blit(background_color, (0, 0))
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                pass
+            if event.key == pygame.K_DOWN:
+                pass
+            if event.key == pygame.K_RIGHT:
+                pass
+            if event.key == pygame.K_LEFT:
+                pass
+    camera.update(player)
+    for sprite in all_sprites:
+        camera.apply(sprite)
+    all_sprites.draw(screen)
+    pygame.display.flip()
+pygame.quit()
